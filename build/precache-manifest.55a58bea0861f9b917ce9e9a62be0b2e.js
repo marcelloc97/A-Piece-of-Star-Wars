46d5ec1d6e91a6a70c80bfce7a3efc386c7e457e
marcelloc97 <marcellocontact97@gmail.com>
2020-06-01 18:37:12 -0300

    release v1.0

---
+++ build/precache-manifest.55a58bea0861f9b917ce9e9a62be0b2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "7bf06cef75498b6293f769b23059c6f5",
+    url: "/index.html",
+  },
+  {
+    revision: "fff5010fb68e22e61df8",
+    url: "/static/css/main.d60bdee4.chunk.css",
+  },
+  {
+    revision: "d2fe79566b4705095583",
+    url: "/static/js/2.ba9de96f.chunk.js",
+  },
+  {
+    revision: "4cba152bee924c40068cb5f841f6f027",
+    url: "/static/js/2.ba9de96f.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "fff5010fb68e22e61df8",
+    url: "/static/js/main.ebe8a5ec.chunk.js",
+  },
+  {
+    revision: "195b67676c9a16659fc1",
+    url: "/static/js/runtime-main.a21a960b.js",
+  },
+  {
+    revision: "738e69d72b6e716224108138e159c726",
+    url: "/static/media/background-stars-desktop_81342104.738e69d7.webp",
+  },
+  {
+    revision: "6dcff896e5e6b03d7dfd06fa98f77d83",
+    url: "/static/media/logo.6dcff896.svg",
+  },
+]);
